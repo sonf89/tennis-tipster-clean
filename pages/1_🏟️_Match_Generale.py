3c6791cda9b73f965e56c410f7b32156cd7c249d
sonf89 <radisifabio@gmail.com>
2025-10-02 10:01:25 +0200

    Update and rename 1_🍽️_Match_Generale.py to 1_🏟️_Match_Generale.py

--- pages/1_🏟️_Match_Generale.py
+++ pages/1_🏟️_Match_Generale.py
@@ -2,6 +2,6 @@ import streamlit as st
 from utils import ensure_session, reset_all, render_stats_editor
 
 ensure_session()
-st.title("🍽️ Match Generale")
+st.title("🏟️ Match Generale")
 if st.button("🔄 Reset TOTALE", use_container_width=True):
     reset_all(); st.success("Azzerato."); st.stop()
